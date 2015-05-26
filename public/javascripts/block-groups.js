b3b3d1a0dfe3c044e3398d56896eab558e8fdd5c
Justin Martinez <jmartinez@drcog.org>
2015-05-26 17:02:26 -0600

    Data query functionality is working

--- public/javascripts/block-groups.js
+++ public/javascripts/block-groups.js
@@ -1,11 +1,196 @@
 /**
  * Created by JMartinez on 5/21/2015.
  */
+
+//var pg = require('pg');
+//var conString = "postgres://postgres:postgres@localhost/postgres"
+
 app.controller('block-groups-ctrl', function($scope, $http){
 
-    var map = new L.map("mapCanvas", {center:[39.75, -104.95], zoom:10})
-        .addLayer(new L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'));
+    //scope variables
 
-    map.setView([39.75, -104.95], 10);
+    $scope.dataTable = ['Demographics', 'Income'];
+    $scope.selectedDataTable = $scope.dataTable[0];
+
+
+    //get input control data
+
+    $scope.$watch('selectedDataTable', function(nv,ov){
+        if($scope.selectedDataTable == 'Demographics'){
+            //request demographic input control data
+            $http.get('/races')
+                .success(function(response){
+                    var out = [];
+                    response.forEach(function(cv, index, arr){
+                        out.push(cv.race);
+                    });
+                    $scope.categories = out;
+                });
+            $http.get('/age-sex-grp')
+                .success(function(response){
+                    var out = [];
+                    response.forEach(function(cv, index, arr){
+                        out.push(cv.age_sex_grp);
+                    });
+                    $scope.ageGrp = out;
+                });
+
+        }
+        else{
+            //request income input control data
+            $http.get('/income-grp')
+                .success(function(response){
+                    var out = [];
+                    response.forEach(function(cv, index, arr){
+                        out.push(cv.income_grp);
+                    });
+                    $scope.categories = out;
+                });
+            $http.get('/age-grp')
+                .success(function(response){
+                    var out = [];
+                    response.forEach(function(cv, index, arr){
+                        out.push(cv.age_grp);
+                    });
+                    $scope.ageGrp = out;
+                });
+        }
+
+    })
+
+
+    $scope.query = function(){
+       if($scope.selectedDataTable === 'Demographics'){
+           var dataToSend = {race: $scope.selectedCategory, ageGrp: $scope.selectedAgeGrp};
+
+           $http.post('/query-demographics', dataToSend)
+               .success(function(response){
+                   colorMap(response); //call d3 function that will color map
+               })
+
+       }
+        else{
+           var dataToSend = {race: $scope.selectedCategory, ageGrp: $scope.selectedAgeGrp};
+
+           $http.post('/query-income', dataToSend)
+               .success(function(response){
+                   colorMap(response); //call d3 function that will color map
+               })
+       }
+
+    }
+
+    //build map
+    var map = L.map('mapCanvas').setView([39.75, -104.95], 10);
+
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+    var drawnItems = new L.FeatureGroup();
+    map.addLayer(drawnItems);
+
+    var drawControl = new L.Control.Draw({
+        draw: {
+            position: 'topleft',
+            polygon: {
+                title: 'Draw a sexy polygon!',
+                allowIntersection: false,
+                drawError: {
+                    color: '#b00b00',
+                    timeout: 1000
+                },
+                shapeOptions: {
+                    color: '#bada55'
+                },
+                showArea: true
+            },
+            polyline: {
+                metric: false
+            },
+            circle: {
+                shapeOptions: {
+                    color: '#662d91'
+                }
+            }
+        },
+        edit: {
+            featureGroup: drawnItems
+        }
+    });
+    map.addControl(drawControl);
+
+    map.on('draw:created', function (e) {
+        var type = e.layerType,
+            layer = e.layer;
+
+        if (type === 'marker') {
+            layer.bindPopup('A popup!');
+        }
+
+        drawnItems.addLayer(layer);
+    });
+
+    //load data
+
+    function action(){
+
+        queue()
+            .defer(d3.json, 'data/block-group.json')
+            .await(loadShapes)
+
+    }
+
+    var svg = d3.select(map.getPanes().overlayPane).append("svg");
+    var g = svg.append("g").attr("class", "leaflet-zoom-hide");
+    var mapById = d3.map();
+    var quantize = d3.scale.quantize()
+        .range(d3.range(7).map(function(i){return "q" + i + "-7";}));
+
+    function loadShapes(error, blockGrp){
+
+        var shape = topojson.feature(blockGrp, blockGrp.objects.bg10);
+
+        var transform = d3.geo.transform({point: projectPoint}),
+            path = d3.geo.path().projection(transform);
+
+        var feature = g.selectAll("path")
+            .data(shape.features)
+            .enter()
+            .append("path")
+            .attr("class", "block-groups");
+
+
+        map.on("viewreset", reset);
+        reset();
+
+        function reset() {
+            var bounds = path.bounds(shape), topLeft = bounds[0],
+                bottomRight = bounds[1];
+
+            svg .attr("width", bottomRight[0] - topLeft[0])
+                .attr("height", bottomRight[1] - topLeft[1])
+                .style("left", topLeft[0] + "px")
+                .style("top", topLeft[1] + "px");
+
+            g   .attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+
+            feature.attr("d", path);
+
+
+        }
+
+        function projectPoint(x, y) {
+            var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+            this.stream.point(point.x, point.y);
+        }
+
+    }
+
+    action();
+
+    //variables for map classification
+
+
+
+
 
 });
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var conString = "postgres://postgres:postgres@localhost/postgres"
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,5 +18,152 @@ router.get('/aging-demographics', function(req, res, next){
 
 router.get('/block-groups', function(req, res, next){
     res.render('block-groups', {title: 'Block Groups'});
+});
+
+router.get('/races', function(req, res, next){
+    pg.connect(conString, function(err, client, done){
+        if(err){
+            return console.error('error fetching client from pool', err);
+        }
+        client.query('select distinct race from demographics order by race', function(err,result){
+            done();
+            if(err){
+                return console.error('error running query', err);
+            }
+            var out = result.rows;
+            res.send(out);
+        });
+    });
+});
+
+router.get('/age-sex-grp', function(req, res, next){
+    pg.connect(conString, function(err, client, done){
+        if(err){
+            return console.error('error fetching client from pool', err);
+        }
+        client.query('select distinct age_sex_grp from demographics order by age_sex_grp', function(err,result){
+            done();
+            if(err){
+                return console.error('error running query', err);
+            }
+            var out = result.rows;
+            res.send(out);
+        });
+    });
+});
+
+router.get('/age-grp', function(req, res, next){
+    pg.connect(conString, function(err, client, done){
+        if(err){
+            return console.error('error fetching client from pool', err);
+        }
+        client.query('select distinct age_grp from income order by age_grp', function(err,result){
+            done();
+            if(err){
+                return console.error('error running query', err);
+            }
+            var out = result.rows;
+            res.send(out);
+        });
+    });
+});
+
+router.get('/income-grp', function(req, res, next){
+    pg.connect(conString, function(err, client, done){
+        if(err){
+            return console.error('error fetching client from pool', err);
+        }
+        client.query('select distinct income_grp from income order by income_grp', function(err,result){
+            done();
+            if(err){
+                return console.error('error running query', err);
+            }
+            var out = result.rows;
+            res.send(out);
+        });
+    });
+});
+
+
+router.post('/query-demographics', function(req, res, next){
+    pg.connect(conString, function(err, client, done){
+        if(err){
+            return console.error('error fetching client from pool', err);
+        }
+
+        //needs this funky & hackish code because node.js pg has difficulty with arrays passed as parameters
+        //also want to get the postgres parameterized escaping (for security and best practices)
+        var raceArr = req.body.race;
+        var ageArr = req.body.ageGrp;
+
+        var params1 = [];
+        var params2 = [];
+        for(var i= 1; i<=raceArr.length; i++){
+            params1.push('$' + i);
+        }
+
+        for(var j= params1.length + 1; j<= ageArr.length + params1.length; j++){
+            params2.push('$' + j);
+        }
+
+        var raceAge = raceArr.concat(ageArr);
+
+        var queryText = 'SELECT blk_grps."GEOid2", blk_grps.county, sum(value) as "grp_tot", tot, sum(value)/nullif(tot,0) as "pct" ' +
+            'from blk_grps, demographics where blk_grps."GEOid2" = demographics."GEOid2" AND race IN(' + params1.join(',') +
+            ') AND age_sex_grp IN(' + params2.join(',') + ') GROUP BY blk_grps."GEOid2", blk_grps.county, blk_grps.tot';
+
+        client.query(queryText,raceAge,
+            function(err,result){
+            done();
+            if(err){
+                return console.error('error running query', err);
+            }
+            var out = result.rows;
+            res.send(out);
+        });
+    });
 })
+
+router.post('/query-income', function(req, res, next){
+    pg.connect(conString, function(err, client, done){
+        if(err){
+            return console.error('error fetching client from pool', err);
+        }
+
+        //needs this funky & hackish code because node.js pg has difficulty with arrays passed as parameters
+        //also want to get the postgres parameterized escaping (for security and best practices)
+        var raceArr = req.body.race;
+        var ageArr = req.body.ageGrp;
+
+        var params1 = [];
+        var params2 = [];
+        for(var i= 1; i<=raceArr.length; i++){
+            params1.push('$' + i);
+        }
+
+        for(var j= params1.length + 1; j<= ageArr.length + params1.length; j++){
+            params2.push('$' + j);
+        }
+
+        var raceAge = raceArr.concat(ageArr);
+
+        var queryText = 'SELECT blk_grps."GEOid2", blk_grps.county, sum(value) as "grp_tot", tot, sum(value)/nullif(tot,0) as "pct" ' +
+            'from blk_grps, income where blk_grps."GEOid2" = income."GEOid2" AND income_grp IN(' + params1.join(',') +
+            ') AND age_grp IN(' + params2.join(',') + ') GROUP BY blk_grps."GEOid2", blk_grps.county, blk_grps.tot';
+
+        client.query(queryText,raceAge,
+            function(err,result){
+                done();
+                if(err){
+                    return console.error('error running query', err);
+                }
+                var out = result.rows;
+                res.send(out);
+            });
+    });
+})
+
+
+
+
 module.exports = router;
